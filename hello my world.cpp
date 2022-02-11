9cda825943e0107fc2b7e3dcd640ea701f5f97a0
Muhammad Danial Yousaf <99211190+daniyalyousuf48@users.noreply.github.com>
2022-02-11 22:52:17 +0500

    Add files via upload

---
+++ hello my world.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ int main()
+{
+	int a=2;
+	char b='A';
+	float d=3.13;
+printf("%d" ,a);
+printf("\n%c" ,b);
+printf("\n%2f" ,d);
+	printf("\nmy name is daniyal");
+	printf("\nhello,world");
+	return 0;
+}
+
